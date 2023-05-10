ca3278bf8e33237c8c82815347adda48b551bafa
jakekressley <kressleyjake@gmail.com>
2023-05-10 13:56:31 -0400

    got drag functionality

--- script.js
+++ script.js
@@ -1,33 +1,101 @@
-const container = document.querySelector('#container');
+const gridContainer = document.querySelector('#grid-container');
 const defaultValue = 16;
 
 function createDivs(input) {
     for (let i = 0; i < input; i++) {
         const colDiv = document.createElement('div');
-        container.appendChild(colDiv);
+        gridContainer.appendChild(colDiv);
        
         for (let j = 0; j < input; j++) {
+            const gridContainerWidth = document.querySelector('#grid-container').getBoundingClientRect().width
+            const gridContainerHeight = document.querySelector('#grid-container').getBoundingClientRect().height
+
             const rowDiv = document.createElement('div');
-            rowDiv.className = "row";
-            rowDiv.style.backgroundColor = 'red';
-            rowDiv.style.width = 320 / input - 2 + 'px';
-            rowDiv.style.height = 320 / input - 2 + 'px';
+            rowDiv.classList.add('row')
+            rowDiv.style.width = gridContainerWidth / input - 2 + 'px';
+            rowDiv.style.height = gridContainerHeight / input - 2 + 'px';
             rowDiv.style.border = '1px solid black';
+            rowDiv.draggable = 'true';
             colDiv.appendChild(rowDiv);
         }
     }
 }
 
-function updateDivs(val) {
+let colorArray = [
+    ['#FF0000', '#00FFFF', '#FF00FF'],
+    ['#FF8000', '#0080FF', '#964B00'],
+    ['#FFFF00', '#0000FF', '#FFFFFF'],
+    ['#00FF00', '#8000FF', '#000000']
+]
+
+let currentColor = 'black';
+
+const colorSection = document.querySelector('#color-section')
+for (let i = 0; i < 4; i++) {
+    const colDiv = document.createElement('div');
+    colorSection.append(colDiv);
+    for (let j = 0; j < 3; j++) {
+        const rowDiv = document.createElement('div')
+        rowDiv.classList.add('color-div')
+        rowDiv.style.backgroundColor = colorArray[i][j];
+        rowDiv.addEventListener('click', () => {
+            console.log('balls')
+        })
+        colDiv.append(rowDiv);
+    }
+}
+
+createDivs(defaultValue);
+newEventListeners();
+
+const divs = document.querySelectorAll('.row')
+    divs.forEach((div) => {
+        div.addEventListener('dragover', (e) => {
+            e.preventDefault()
+            changeColor(div, currentColor)
+        });
+    })
+
+function newEventListeners() {
+    const divs = document.querySelectorAll('.row')
+    divs.forEach((div) => {
+        div.addEventListener('dragover', (e) => {
+            e.preventDefault()
+            changeColor(div)
+        });
+    })   
+}
+
+
+function updateGridSize(val) {
     document.querySelector('#slider-value').value = val;
-    container.innerHTML = "";
+    gridContainer.innerHTML = "";
     createDivs(val);
+    newEventListeners()
 }
 
 function updateSlider(val) {
     document.querySelector('#slider').value = val
-    updateDivs(val)
+    updateGridSize(val)
 }
 
+function changeColor(div, color) {
+    div.style.backgroundColor = color;
+}
 
-createDivs(defaultValue);
+let colorSelected = false
+
+function getCurrentColor(div) {
+    if (!colorSelected) return 'black'
+
+    const colorDivs = document.querySelectorAll('.color-div')
+    console.log(color-divs)
+    colorDivs.forEach((colorDiv) => {
+        div.addEventListener('click', () => {
+            console.log("different color pressed")
+            colorSelected = true
+            const color = colorDiv.style.backgroundColor
+            return color;
+        });
+    })
+}
